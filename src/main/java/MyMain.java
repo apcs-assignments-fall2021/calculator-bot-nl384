53bfb4f17a1cae1548537389b6366806091d4e2e
Nathan Lin <nlin@athenian.org>
2021-09-02 09:03:48 -0700

    Finished :)

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -2,29 +2,51 @@ import java.util.Scanner;
 
 public class MyMain {
 
-    // Calculates the median of the three inputs
     public static int median(int a, int b, int c) {
-        return 0; // REPLACE WITH YOUR CODE
+        return a + b + c - Math.min(Math.min(a, b), c) - Math.max(Math.max(a, b), c);
     }
 
-    // Returns the input with the larger absolute value
     public static int magnitude(int a, int b) {
-        return 0; // REPLACE WITH YOUR CODE
+        if (Math.abs(a) > Math.abs(b)) {
+            return a;
+        }
+        else {
+            return b;
+        }
     }
 
-    // Returns the "c" value from the Pythagorean theorem "a^2 + b^2 = c^2",
-    // where "a" and "b" are the inputs to the method
     public static double pythagoras(int a, int b) {
-        return 0.0; // REPLACE WITH YOUR CODE
+        return Math.sqrt(a*a + b*b);
     }
 
     public static void main(String[] args) {
-        // You may want to keep these lines of code to test that your methods work
-        System.out.println(median(1, 2, 3)); // should be 2
-        System.out.println(magnitude(-7, -1)); // should be -7
-        System.out.println(pythagoras(3, 4)); // should be 5.0
-
         Scanner scan = new Scanner(System.in);
-        // YOUR CODE HERE
+        System.out.println("What command would you like to carry out?");
+        String command = scan.next();
+
+        if (command.equals("median")) {
+            System.out.println("What is the first number?");
+            int x = scan.nextInt();
+            System.out.println("What is the second number?");
+            int y = scan.nextInt();
+            System.out.println("What is the third number?");
+            int z = scan.nextInt();
+
+            System.out.println("The median is " + median(x, y, z));
+        }
+        else if (command.equals("magnitude")) {
+            System.out.println("What is the first number?");
+            int x = scan.nextInt();
+            System.out.println("What is the second number?");
+            int y = scan.nextInt();
+            System.out.println("The number with the larger magnitude is " + magnitude(x, y));
+        }
+        else if (command.equals("pythagoras")) {
+            System.out.println("What is the first number?");
+            int x = scan.nextInt();
+            System.out.println("What is the second number?");
+            int y = scan.nextInt();
+            System.out.println("The third side of that triangle is " + pythagoras(x, y));
+        }
     }
 }
